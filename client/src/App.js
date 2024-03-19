e5d354a4cbed2cda32b4d3aa39940b283d0f0710
Seeker223 <baduntobi2024@gmail.com>
2024-03-19 15:16:47 +0100

    created first chat ui with chat engine

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="179a6c86-e5fe-4162-b059-29d5a15d9367"
+        userName="baduntobi"
+        userSecret="Bankaiichigo2024"
+        />
+    )
+}
+
+export default App;
